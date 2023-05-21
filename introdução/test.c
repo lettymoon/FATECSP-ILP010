8c3e83ee2454d42d0740c32e113e8e734fd8c1b7
lettymoon <lettinwonderland@gmail.com>
2023-05-21 14:57:52 -0300

    deletando organização antiga

--- introdução/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio>
-
-int main(void){
-
-    printf("hello world");
-
-
-    return 0;
-}
--- macros-funcoes/exempl03.c
+++
@@ -1,9 +0,0 @@
-
-
-#include <stdio.h>
-#define quadrado(n) n*n
-
-    int main(void) {
-    printf("%d\n",quadrado(2)); // 4 (certo!)
-    return 0;
-}
